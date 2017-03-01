e91d17fa159d7ac7d375976117426811cdd6f860
Karim <kabdeselam@synergicpartners.com>
2017-03-01 14:45:27 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+
+import "d3";
+import "nvd3";
+import { nvD3 } from "ng2-nvd3";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    nvD3
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
+  ],
+  exports: [
+      nvD3
   ],
   providers: [],
   bootstrap: [AppComponent]
